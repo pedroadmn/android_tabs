046f47545e70f4aee8b2fb576053c58428cd5ec2
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2020-12-06 18:30:32 -0300

    adding method to play related sounds

--- app/src/main/java/com/example/tabs/Fragments/AnimalsFragment.java
+++ app/src/main/java/com/example/tabs/Fragments/AnimalsFragment.java
@@ -1,5 +1,6 @@
 package com.example.tabs.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.tabs.R;
 
-public class AnimalsFragment extends Fragment {
+public class AnimalsFragment extends Fragment implements View.OnClickListener{
+
+    private ImageButton ibDog;
+    private ImageButton ibCat;
+    private ImageButton ibLion;
+    private ImageButton ibDonkey;
+    private ImageButton ibSheep;
+    private ImageButton ibCow;
+
+    private MediaPlayer mediaPlayer;
 
     public AnimalsFragment() {
     }
@@ -23,6 +34,69 @@ public class AnimalsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_animals, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_animals, container, false);
+
+        ibDog = view.findViewById(R.id.ibDog);
+        ibCat = view.findViewById(R.id.ibCat);
+        ibLion = view.findViewById(R.id.ibLion);
+        ibDonkey = view.findViewById(R.id.ibDonkey);
+        ibSheep = view.findViewById(R.id.ibSheep);
+        ibCow = view.findViewById(R.id.ibCow);
+
+        ibDog.setOnClickListener(this);
+        ibCat.setOnClickListener(this);
+        ibLion.setOnClickListener(this);
+        ibDonkey.setOnClickListener(this);
+        ibSheep.setOnClickListener(this);
+        ibCow.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ibCat:
+                mediaPlayer = createMediaPlayer(R.raw.cat);
+                break;
+            case R.id.ibLion:
+                mediaPlayer = createMediaPlayer(R.raw.monkey);
+                break;
+            case R.id.ibDonkey:
+                mediaPlayer = createMediaPlayer(R.raw.lion);
+                break;
+            case R.id.ibSheep:
+                mediaPlayer = createMediaPlayer(R.raw.sheep);
+                break;
+            case R.id.ibCow:
+                mediaPlayer = createMediaPlayer(R.raw.cow);
+                break;
+            default:
+                mediaPlayer = createMediaPlayer(R.raw.dog);
+                break;
+        }
+        playSound();
+    }
+
+    private MediaPlayer createMediaPlayer(int soundId) {
+        return MediaPlayer.create(getActivity(), soundId);
+    }
+
+    private void playSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(MediaPlayer::release);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/tabs/Fragments/NumbersFragment.java
+++ app/src/main/java/com/example/tabs/Fragments/NumbersFragment.java
@@ -1,5 +1,6 @@
 package com.example.tabs.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.tabs.R;
 
-public class NumbersFragment extends Fragment {
+public class NumbersFragment extends Fragment implements View.OnClickListener {
+
+    private ImageButton ibOne;
+    private ImageButton ibTwo;
+    private ImageButton ibThree;
+    private ImageButton ibFour;
+    private ImageButton ibFive;
+    private ImageButton ibSix;
+
+    private MediaPlayer mediaPlayer;
 
     public NumbersFragment() {
     }
@@ -23,7 +34,69 @@ public class NumbersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numbers, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_animals, container, false);
+
+        ibOne = view.findViewById(R.id.ibOne);
+        ibTwo = view.findViewById(R.id.ibTwo);
+        ibThree = view.findViewById(R.id.ibThree);
+        ibFour = view.findViewById(R.id.ibFour);
+        ibFive = view.findViewById(R.id.ibFive);
+        ibSix = view.findViewById(R.id.ibSix);
+
+        ibOne.setOnClickListener(this);
+        ibTwo.setOnClickListener(this);
+        ibThree.setOnClickListener(this);
+        ibFour.setOnClickListener(this);
+        ibFive.setOnClickListener(this);
+        ibSix.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ibTwo:
+                mediaPlayer = createMediaPlayer(R.raw.two);
+                break;
+            case R.id.ibThree:
+                mediaPlayer = createMediaPlayer(R.raw.three);
+                break;
+            case R.id.ibFour:
+                mediaPlayer = createMediaPlayer(R.raw.four);
+                break;
+            case R.id.ibFive:
+                mediaPlayer = createMediaPlayer(R.raw.five);
+                break;
+            case R.id.ibSix:
+                mediaPlayer = createMediaPlayer(R.raw.six);
+                break;
+            default:
+                mediaPlayer = createMediaPlayer(R.raw.dog);
+                break;
+        }
+        playSound();
+    }
+
+    private MediaPlayer createMediaPlayer(int soundId) {
+        return MediaPlayer.create(getActivity(), soundId);
+    }
+
+    private void playSound() {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(MediaPlayer::release);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        super.onDestroy();
     }
 }
